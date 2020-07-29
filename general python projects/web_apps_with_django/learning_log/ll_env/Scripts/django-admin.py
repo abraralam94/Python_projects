5b8abd7a567c1b5f4e8cdb838016bbc9f5619847
AbrarAlam <41845775+abraralam94@users.noreply.github.com>
2020-07-29 10:37:18 -0400

    upload all generic python apps

---
+++ general python projects/web_apps_with_django/learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\xxxxx\onedrive\documents\python_projects\projects_from_crash_course_in_python\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
